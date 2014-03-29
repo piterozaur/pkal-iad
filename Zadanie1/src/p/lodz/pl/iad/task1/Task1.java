4e47568a7422d9feeacc06be832426514fa759a1
Piotr Kluch <piterozaur@gmail.com>
2014-03-29 05:14:11 +0100

    Fix file saving and finishing some things

--- Zadanie1/src/p/lodz/pl/iad/task1/Task1.java
+++ Zadanie1/src/p/lodz/pl/iad/task1/Task1.java
@@ -22,24 +22,28 @@ import p.lodz.pl.iad.task1.gui.*;
  */
 public class Task1 {
 
+	/*
 	private static final String USER_DIR = System.getProperty("user.dir");
 	
     private static final String SAMPLE_PATH = USER_DIR + "/data/sample.dat";
     private static final String SEPARATOR = ",";
     private static final String STATISTICS_PATH = USER_DIR + "/data/stats.txt";
     private static final String HISTOGRAM_PATH = USER_DIR + "/data";
-        
+    */
+	
     public static void main(String[] args) {
         
+    	/*
         Map<Integer, Map<String, List<Double>>> dataMaps = FileHelper.readDataFromFile(SAMPLE_PATH, SEPARATOR);
         StatisticsHelper statisticsHelper = new StatisticsHelper();
         
         Map<Integer, Map<String, Map<String, Double>>> statistics = statisticsHelper.getAllStatiscticsFromDataSets(dataMaps);
         String statisticsString = FileHelper.saveStatisticsFromDataSets(statistics, STATISTICS_PATH);
-        
+        */
+    	
         //System.out.print(statisticsString);
         
-        System.out.println(statistics.get(0).get("Iris-virginica").get("Median"));
+        //System.out.println(statistics.get(0).get("Iris-virginica").get("Median"));
         //System.out.print(Constants.);
         
         //Draws histograms
@@ -57,7 +61,8 @@ public class Task1 {
 					
 					//Turn off irritating boldness of swing metal theme
 					UIManager.put("swing.boldMetal", Boolean.FALSE); 
-
+	                //UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+					
 					//Run the main application controller
 					initialWindow = new InitialWindow();
 					initialWindow.setVisible(true);
